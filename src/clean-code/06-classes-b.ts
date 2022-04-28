d41785bebf04951d574a36e3db05477b759aa3c9
Alex Meregildo <alexmeco95@gmail.com>
2022-04-28 01:38:16 -0500

    class 6-b

--- src/clean-code/06-classes-b.ts
+++ src/clean-code/06-classes-b.ts
@@ -1,60 +1,95 @@
 (()=>{
  
     //No aplicando el principio de responsabilidad única
-
     type Gender = 'M'|'F';
 
+interface PersonProps{
+    name      : string;
+    gender    : Gender;
+    birthdate : Date;
+}
+
 class Person{
-        constructor(
-           public name:string, 
-           public gender:Gender, 
-           public birthdate:Date){}
+    
+    public name:string; 
+    public gender:Gender; 
+    public birthdate:Date;
+
+        constructor({name, gender, birthdate}: PersonProps){
+            this.name      = name;
+            this.gender    = gender;
+            this.birthdate = birthdate;
+        }
 
     }
  
+   interface UserProps{
+     birthdate :Date;  
+     email     :string;
+     gender    :Gender;
+     name      :string;
+     role      :string;
+   } 
     class User extends Person{
-        public lastAccess: Date;
+        public email : string;
+        public role  : string;
+        public lastAccess : Date; 
 
-        constructor(
-            public email:string,
-            public role:string,
-            name: string,
-            gender: Gender,
-            birthdate: Date,
-            ){
-                super(name, gender, birthdate);
-                this.lastAccess = new Date(); 
-            }
-            checkCredentials(){
-               return true;
-            };
-
+        constructor({
+            birthdate, 
+            email, 
+            gender, 
+            name,
+            role,}: UserProps){
+           super({name, gender, birthdate});
+           this.lastAccess = new Date();
+           this.email = email;
+           this.role = role; 
+         
     }
 
+
+}
+
+interface UserSettingsProps{
+    birthdate          : Date; 
+    email              : string;
+    gender             : Gender;
+    lastOpenFolder     : string;
+    name               : string;
+    role               : string;
+    workingDirectory   : string;
+}
     class UserSettings extends User{
-          constructor(
-               public workingDirectory: string,
-               public lastOpenFolder: string,
-               email    : string,
-               role     : string,
-               name     : string,
-               gender   : Gender,
-               birthdate: Date     
-          ){
-              super(email, role, name, gender, birthdate);
+
+          public workingDirectory: string;
+          public lastOpenFolder: string;
+
+          constructor({
+               workingDirectory,
+               lastOpenFolder,
+               email,
+               role,
+               name,
+               gender,
+               birthdate,
+          }: UserSettingsProps){
+              super({email, role, name, gender, birthdate});
+              this.workingDirectory = workingDirectory;
+              this.lastOpenFolder = lastOpenFolder;
           }    
     }
 
-    const userSettings = new UserSettings(
-          '/usr/home',
-          '/home',
-          'alexmeco@gmail.com',
-          'admin',
-          'Alex',
-          'M',
-          new Date('1990- 01-01')              
-      );
+    const userSettings = new UserSettings({
+          birthdate:new Date('1990- 01-01'),            
+          email:'alexmeco@gmail.com',
+          gender:'M',
+          lastOpenFolder:'/home',
+          name:'Alex',
+          role:'admin',
+          workingDirectory:'/usr/home',
+    });
 
-    console.log({userSettings});
+   console.log({userSettings});
 
 })();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import './clean-code/05-dry.ts';
-import './clean-code/06-classes.ts';
+import './clean-code/06-classes-b.ts';
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
